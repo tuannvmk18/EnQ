eed3daf08437ede4202c429e3526ec9b8bdec6c9
duc <hohuynhduca7bh123@gmail.com>
2020-11-07 23:57:20 +0700

    sidebar bug a little bit

--- src/app/Core/mini-component/mini-component.module.ts
+++ src/app/Core/mini-component/mini-component.module.ts
@@ -6,14 +6,17 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { QuestionFormModule } from 'src/app/Core/Components/question-form/question-form.module';
 @NgModule({
   declarations: [SideBarComponent, NavBarComponent],
   imports: [
     CommonModule,
     MatToolbarModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatSidenavModule,
+    QuestionFormModule
   ],
   exports:[
     NavBarComponent,
