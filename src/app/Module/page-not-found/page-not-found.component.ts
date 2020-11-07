76c6f322a62759babea6d40dcb5aef667be0a27a
duc <hohuynhduca7bh123@gmail.com>
2020-11-07 11:06:18 +0700

    fix something in page-not-found

--- src/app/Module/page-not-found/page-not-found.component.ts
+++ src/app/Module/page-not-found/page-not-found.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageNotFoundComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public router : Router
+  ) { }
 
   ngOnInit(): void {
   }
