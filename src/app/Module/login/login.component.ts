2cf97646ae2576e19f94c9eae663c706cb3b859a
duc <hohuynhduca7bh123@gmail.com>
2020-11-07 15:52:42 +0700

    material bi cai loi gi roi

--- src/app/Module/login/login.component.ts
+++ src/app/Module/login/login.component.ts
@@ -36,4 +36,6 @@ export class LoginComponent implements OnInit {
 
   }
 
+
+
 }
--- src/app/Module/login/login.module.ts
+++ src/app/Module/login/login.module.ts
@@ -4,15 +4,17 @@ import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 @NgModule({
   declarations: [LoginComponent],
   imports: [
     CommonModule,
     LoginRoutingModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule
   ]
 })
 export class LoginModule { }
