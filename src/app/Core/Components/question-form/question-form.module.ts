a1b35369e4afd88c1a54fc5936c40b4baaa37674
NguyenVanMinhTuan <tuan01102000@gmail.com>
2020-11-07 20:04:16 +0700

    upload question dialog

--- src/app/Core/Components/question-form/question-form.module.ts
+++ src/app/Core/Components/question-form/question-form.module.ts
@@ -3,15 +3,17 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { QuestionFormComponent } from './question-form.component';
 import { QuestionEditorComponent } from './question-editor/question-editor.component';
-
-
+import {OverlayModule} from '@angular/cdk/overlay';
+import {MatDialogModule} from '@angular/material/dialog';
 
 @NgModule({
   declarations: [QuestionFormComponent, QuestionEditorComponent],
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    OverlayModule,
+    MatDialogModule
   ],
   exports: [QuestionFormComponent],
   providers: []
