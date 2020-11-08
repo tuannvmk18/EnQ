fe3788cff0abbdd418a38a9f31789a18e1a40dbb
Ferntran <ferntran0214@gmail.com>
2020-11-08 20:59:11 +0700

    check

--- src/app/Core/Components/question-form/question-form.module.ts
+++ src/app/Core/Components/question-form/question-form.module.ts
@@ -5,6 +5,10 @@ import { QuestionFormComponent } from './question-form.component';
 import { QuestionEditorComponent } from './question-editor/question-editor.component';
 import {OverlayModule} from '@angular/cdk/overlay';
 import {MatDialogModule} from '@angular/material/dialog';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [QuestionFormComponent, QuestionEditorComponent],
@@ -13,7 +17,11 @@ import {MatDialogModule} from '@angular/material/dialog';
     ReactiveFormsModule,
     FormsModule,
     OverlayModule,
-    MatDialogModule
+    MatDialogModule,
+    MatStepperModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule
   ],
   exports: [QuestionFormComponent],
   providers: []
--- src/app/Core/mini-component/mini-component.module.ts
+++ src/app/Core/mini-component/mini-component.module.ts
@@ -5,8 +5,9 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { QuestionFormModule } from 'src/app/Core/Components/question-form/question-form.module';
+import { DialogSuccessComponent } from './dialog-success/dialog-success.component';
 @NgModule({
-  declarations: [NavBarComponent],
+  declarations: [NavBarComponent, DialogSuccessComponent],
   imports: [
     CommonModule,
     MatToolbarModule,
