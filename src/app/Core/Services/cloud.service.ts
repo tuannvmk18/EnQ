a13ce408df243cf7a8d69bb16ac4ee4884162084
NguyenVanMinhTuan <tuan01102000@gmail.com>
2020-11-11 22:55:33 +0700

    ko co gi thay doi

--- src/app/Core/Services/cloud.service.ts
+++ src/app/Core/Services/cloud.service.ts
@@ -1,9 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { take } from 'rxjs/operators';
 import { API } from 'src/environments/environment';
-import { reqQuestion } from '../Interfaces/reqQuestion';
 
 @Injectable({
   providedIn: 'root'
@@ -13,38 +11,30 @@ export class CloudService {
   constructor(private http: HttpClient) { }
 
   postQuestion(payload: any): Observable<unknown> {
-    return this.http.post(API + 'test/question', payload).pipe(take(1));
+    return this.http.post(API + 'test/question', payload);
   }
 
   deleteQuestion(id: string): Observable<unknown> {
-    return this.http.delete(API + `test/question/${id}`).pipe(take(1));
+    return this.http.delete(API + `test/question/${id}`);
   }
 
   editQuestionById(id: string, payload: any): Observable<unknown> {
-    return this.http.post(API + `test/question/${id}`, payload).pipe(take(1));
+    return this.http.post(API + `test/question/${id}`, payload);
   }
 
   getQuestionById(id: string): Observable<unknown> {
-    return this.http.get(API + `test/question/${id}`).pipe(take(1));
+    return this.http.get(API + `test/question/${id}`);
   }
 
   getAllQuestion(): Observable<unknown> {
-    return this.http.get(API + 'test/questions').pipe(take(1));
+    return this.http.get(API + 'test/questions');
   }
 
-  // getQuestionByRank(rank): Observable<unknown> {
-  //   return this.http.get(API + `test/test-exam-rank?rank=${rank}`).pipe(take(1));
-  // }
-
-  // getQuestionByType(type): Observable<unknown> {
-  //   return this.http.get(API + `test/test-exam-type?type=${type}`).pipe(take(1));
-  // }
-
   getLeaderByDay(): Observable<unknown> {
-    return this.http.get(API + 'leaders/day').pipe(take(1));
+    return this.http.get(API + 'leaders/day');
   }
 
   getLeaderByWeek(): Observable<unknown> {
-    return this.http.get(API + 'leader/week').pipe(take(1));
+    return this.http.get(API + 'leader/week');
   }
 }
